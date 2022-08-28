51e19649679b4b057e8cd06ca67407117670cf0c
akshayzagad <akshayzagade62@gmail.com.com>
2022-08-28 12:29:08 +0530

    positive negative problem

---
+++ positivenegative.sh
@@ -0,0 +1,12 @@
+read -p "enter the number: " num
+
+           
+            if((  num > 0   ))
+            then
+                echo "Positive"
+            elif((  num < 0 ))
+            then
+                echo "Negative"
+            else
+                echo "Zero"
+            fi
